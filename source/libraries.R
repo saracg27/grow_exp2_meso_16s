95b13bfd56f767d910887409a1b00139165660fe
Sara <sara.correagarcia@gmail.com>
2023-09-19 11:21:30 -0400

    created basic analyses files - not adapted to current data

---
+++ source/libraries.R
@@ -0,0 +1,6 @@
+# Common libraries
+
+#install.packages("pacman")
+pacman::p_load(pacman, colorspace, cowplot, DESeq2, dunn.test, glue,
+               ggdist, ggforce, ggpubr, ggtext, here, labdsv, MASS, psych, 
+               tidyverse, roperators, vegan)
