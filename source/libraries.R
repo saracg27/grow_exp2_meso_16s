b6643bca56e451f2466f0a8d042c55dad74e02a6
Sara <sara.correagarcia@gmail.com>
2023-09-27 16:48:43 -0400

    included EnhancedVolcano and ComplexHeatmap to libraries.R

--- source/libraries.R
+++ source/libraries.R
@@ -1,6 +1,12 @@
 # Common libraries
 
+# if (!requireNamespace('BiocManager', quietly = TRUE))
+#   install_packages('BiocManager')
+
+BiocManager::install('EnhancedVolcano')
+BiocManager::install("ComplexHeatmap")
+
 #install.packages("pacman")
 pacman::p_load(pacman, colorspace, cowplot, DESeq2, dunn.test, glue,
                ggdist, ggforce, ggpubr, ggtext, here, labdsv, MASS, psych, 
-               tidyverse, roperators, vegan)
+               tidyverse, roperators, vegan, EnhancedVolcano, ComplexHeatmap)
