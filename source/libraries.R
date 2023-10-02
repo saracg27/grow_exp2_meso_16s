71e432f91c2d09c056dd5554b2b89b5b7c2ab877
Sara <sara.correagarcia@gmail.com>
2023-10-02 15:04:30 -0400

    commented biocManager::install lines

--- source/libraries.R
+++ source/libraries.R
@@ -3,8 +3,8 @@
 # if (!requireNamespace('BiocManager', quietly = TRUE))
 #   install_packages('BiocManager')
 
-BiocManager::install('EnhancedVolcano')
-BiocManager::install("ComplexHeatmap")
+#BiocManager::install('EnhancedVolcano')
+#BiocManager::install("ComplexHeatmap")
 
 #install.packages("pacman")
 pacman::p_load(pacman, colorspace, cowplot, DESeq2, dunn.test, glue,
