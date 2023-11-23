c338d97fce8af4410f6ba820342297ea6369f70a
Sara <sara.correagarcia@gmail.com>
2023-11-23 18:45:00 -0500

    added pairwise comparisons in 18S PERMANOVA

--- source/SM_Water_18S_03_BetaDiv.R
+++ source/SM_Water_18S_03_BetaDiv.R
@@ -49,7 +49,7 @@ write.table(Permanova.rclr,row.names=T,sep=";",here("Results","Tables","18S_Wate
 # Permanova_table[,c(2:4)] <- round(Permanova_table[,c(2:4)],2)
 # table.p <- ggtexttable(Permanova_table, rows = NULL)
 
-<<<<<<< HEAD
+
 
 # The two treatments and the time have significant interactions between each other.
 # Calculate PERMANOVA on glom treatments and identify significant interactions with pairwise.adonis
@@ -58,27 +58,58 @@ metadata$time_temp = paste0(metadata$Time, "_", metadata$Temperature)
 metadata$plant_time = paste0(metadata$Sample_type, "_", metadata$Time)
 
 Permanova.plant_temp <- adonis2(rclr_dist_matrix~plant_temp,
-                        data = metadata, permutations = 999)
-Permanova.plant_temp
-pairwise.adonis(rclr_dist_matrix, metadata$plant_temp)
-
-Permanova.time_temp <- adonis2(rclr_dist_matrix~time_temp,
                                 data = metadata, permutations = 999)
+Permanova.plant_temp
+pairwise.Permanova.plant_temp <- pairwise.adonis(rclr_dist_matrix, metadata$plant_temp)
+write.table(pairwise.Permanova.plant_temp,
+            row.names=F,sep="\t",here("output","tables","18S_Water_RCLR_pairwise.Permanova.plant_temp.csv"))
+Permanova.time_temp <- adonis2(rclr_dist_matrix~time_temp,
+                               data = metadata, permutations = 999)
 Permanova.time_temp
-pairwise.adonis(rclr_dist_matrix, metadata$time_temp)
-)
+pairwise.Permanova.time_temp<-pairwise.adonis(rclr_dist_matrix, metadata$time_temp)
+write.table(pairwise.Permanova.time_temp,
+            row.names=F,sep="\t",here("output","tables","18S_Water_RCLR_pairwise.Permanova.time_temp.csv"))
 
 Permanova.plant_time <- adonis2(rclr_dist_matrix~plant_time,
                                 data = metadata, permutations = 999)
 Permanova.plant_time
-pairwise.adonis(rclr_dist_matrix, metadata$plant_time)
+pairwise.Permanova.plant_time <- pairwise.adonis(rclr_dist_matrix, metadata$plant_time)
+write.table(pairwise.Permanova.plant_time,
+            row.names=F,sep="\t",here("output","tables","18S_Water_RCLR_pairwise.Permanova.plant_time.csv"))
+
+# Define the list of small strings you want to check for
+
+to_keep <- c("No_plant_D10 vs Triglochin_D10",
+             "No_plant_D10 vs Scirpus_D10",
+             "No_plant_D0 vs Triglochin_D0",
+             "No_plant_D0 vs Scirpus_D0",
+             "No_plant_D16 vs Scirpus_D16",
+             "No_plant_D16 vs Triglochin_D16",
+             "No_plant_D77 vs Triglochin_D77",
+             "No_plant_D77 vs Scirpus_D77",
+             "No_plant_D62 vs Triglochin_D62",
+             "No_plant_D62 vs Scirpus_D62",
+             "No_plant_D35 vs Triglochin_D35",
+             "No_plant_D35 vs Scirpus_D35",
+             "Triglochin_D10 vs Scirpus_D10",
+             "Triglochin_D0 vs Scirpus_D0",
+             "Triglochin_D77 vs Scirpus_D77",
+             "Triglochin_D62 vs Scirpus_D62",
+             "Triglochin_D35 vs Scirpus_D35",
+             "Scirpus_D16 vs Triglochin_D16")
+pairwise.Permanova.plant_time1 <- pairwise.Permanova.plant_time[pairwise.Permanova.plant_time$pairs %in% to_keep, ]
+pairwise.Permanova.plant_time1$padjusted_18 <- p.adjust(pairwise.Permanova.plant_time1$p.value, method = "fdr")
+write.table(pairwise.Permanova.plant_time1,
+            row.names=F,sep="\t",here("output","tables","18S_Water_RCLR_pairwise.Permanova.plant_time_18adjusted.csv"))
+
+
 
 
 
 #### Ordinations 
-=======
+
 #### PCA 
->>>>>>> 8a11300d0ab15e872946566f89446fc45ab6c6dd
+
 ord_rclr <- phyloseq::ordinate(ps_rclr, "RDA", distance = "euclidean")
 
 ## Sample type  
